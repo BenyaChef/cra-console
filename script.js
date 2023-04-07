3c25e4ce1e1275f01cdcb9207af3ed56fa6e68eb
Maxim <benyachef@gmail.com>
2023-04-07 17:26:49 +0300

    yes, this work!

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
  // const i = q => Math.pow(q, 3);
 
- console.log(i(3));
+ // console.log(i(3));
 function getIp() {
 
-}const newArr = [];
+}
+
+const newArr = [2, 3, 4, 5, 6, 7, 8];
+
+
+let arr = newArr.filter(e => e % 2 === 1);
+
+console.log(arr);
