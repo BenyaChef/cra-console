eead95a92792c2ebd67642a06498af94bce19328
Maxim <benyachef@gmail.com>
2023-04-08 14:15:20 +0300

    new branch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 'use strict';
 
- // const i = q => Math.pow(q, 3);
+// const i = q => Math.pow(q, 3);
 
- // console.log(i(3));
+// console.log(i(3));
 function getIp() {
 
 }
@@ -13,6 +13,8 @@ const newArr = [2, 3, 4, 5, 6, 7, 8];
 let arr = newArr.filter(e => e % 2 === 1);
 
 console.log(arr);
-console.log(arr);
-console.log(arr);
-console.log(arr);
+
+
+const lot = t => t * 2;
+
+console.log(lot(2));
