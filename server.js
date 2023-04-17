e64d10f8c0f6c778a473d1abd03764a50be03630
Maxim <benyachef@gmail.com>
2023-04-17 21:20:36 +0300

    ts init

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 'use strict';
-let counter = 0;
-console.log(counter);
+const http = require('http');
+const fs = require('fs');
+
+
+
+
+const server = http.createServer( (request, response) => {
+    switch (request.url) {
+        case '/home': {
+            const data = 'best free online course'
+            response.write(data)
+            response.end()
+            break;
+        }
+        default: {
+            response.write('404 not found')
+            response.end()
+        }
+    }
+});
+
+server.listen(3000);
